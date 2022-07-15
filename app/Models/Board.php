7ca5d477871df5569eeb8565d41b6685344b1ade
sa33drs <sa33drs@gmail.com>
2022-07-15 19:27:06 +0430

    add title and description to Board Model

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -8,5 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Board extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','creator_id'];
+    protected $fillable = ['title','description','creator_id'];
+
+//    public function users(){
+//        return $this->belongsToMany(Board::class);
+//    }
 }

0ff36a79293660c55b3eee9466c0a392d7e2c6a3
sa33drs <sa33drs@gmail.com>
2022-07-19 22:28:41 +0430

    fix bug

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,6 +6,7 @@ use App\Models\Task;
 use App\Models\TaskField;
 use App\Models\TaskMetaData;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TaskController extends Controller
 {
